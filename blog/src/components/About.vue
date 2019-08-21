5bc9c3cc23c98931dd8d56081725684f95eae960
André Rodrigues (Xor) <xor2017@outlook.com>
2019-08-21 20:36:31 -0300

    Upload blog simples

---
+++ blog/src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="galeria">
+    <h1>{{ mensagem }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      mensagem: 'Seja bem vindo ao meu site'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ blog/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <About></About>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import About from '@/components/About.vue'
+
+export default {
+  name: 'home',
+  components: {
+    About
+  }
+}
+</script>
